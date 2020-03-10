795635922dfcc5282f8e768d24fd1793e12e9525
BilliMorgan <shimko20@gmail.com>
2020-03-10 23:49:03 +0000

    completed with @Linh095, @d0ugr

---
+++ timer.js
@@ -0,0 +1,10 @@
+
+
+const timer1 = (timers) => {
+  timers.forEach(element => {
+    setTimeout(() => {
+      process.stdout.write(`\x07${element}s timeout\n`);
+    }, element * 1000);
+  });
+};
+timer1(process.argv.slice(2));
